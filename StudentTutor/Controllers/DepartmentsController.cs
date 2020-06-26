bfe60394ceedcab36531f9ffa7f618676012c889
Lungelo <21711486@ditinfo.ac.za>
2020-06-26 23:54:43 +0200

    Contollers done

---
+++ StudentTutor/Controllers/DepartmentsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentTutor.Models;
+using StudentTutor.Repository;
+
+namespace StudentTutor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentsController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+        [HttpGet]
+        public IEnumerable<Department> GetAll()
+        {
+            return _departmentRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Department>Get(int ? id)
+        {
+            return _departmentRepository.Get(id);
+        }
+
+        [HttpPost]
+        public Department Add(Department department)
+        {
+            _departmentRepository.Add(department);
+            return department;
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            _departmentRepository.Delete(id);
+        }
+
+        [HttpPut]
+        public void Update(Department department)
+        {
+            _departmentRepository.Update(department);
+        }
+
+
+    }
+}
---
+++ StudentTutor/Controllers/FacultiesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentTutor.Models;
+using StudentTutor.Repository;
+
+namespace StudentTutor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacultiesController : ControllerBase
+    {
+        private readonly IFAcultyRepository _facultyRepository;
+
+        public FacultiesController (IFAcultyRepository facultyRepository)
+        {
+            _facultyRepository = facultyRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Faculty> GetAll()
+        {
+           return _facultyRepository.Get();
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Faculty>Get(int ? id)
+        {
+            return _facultyRepository.Get(id);
+        }
+        [HttpPost]
+        public Faculty Add(Faculty faculty)
+        {
+            _facultyRepository.Add(faculty);
+            return faculty;
+        }
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            _facultyRepository.Delete(id);
+        }
+        [HttpPut]
+        public void Update(Faculty faculty)
+        {
+            _facultyRepository.Update(faculty);
+        }
+    }
+}
